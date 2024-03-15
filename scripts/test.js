0aa5ecbd8593155916019671443197b507507cbb
Cabraa11 <140258420+Cabraa11@users.noreply.github.com>
2024-03-14 18:50:15 -0600

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -207,6 +207,48 @@ cancelmodal.addEventListener("click", function () {
 const circleButton = document.querySelector(".sexyCircle");
 const overallButton = document.querySelector(".DarkWhiteModeDiv");
 const htmlBody = document.querySelector("html");
+const headerBorder = document.querySelector(".header-div");
+const guessElonTitle = document.querySelector(".guess-elon-money");
+const netWorthTitle = document.querySelector(".title1");
+const elonSummary = document.querySelector(".elon-summary");
+const elonMoreInfoLink = document.querySelector(".more-info-elon-wiki");
+const elonProfileX = document.querySelector(".x-logo-elon-profile");
+const divider = document.querySelector(".divider");
+const trueFalseDiv = document.querySelector(
+  ".true-false-buttons-guess-elon-money-div"
+);
+const life30Div = document.querySelector(".life30-div");
+const onWarDiv = document.querySelector(".OnWar-div");
+const superintelligenceDiv = document.querySelector(".Superintelligence-div");
+const life30AuthorProfile = document.querySelector(".author-life30-div-circle");
+const onWarAuthorProfile = document.querySelector(
+  ".carl-von-clausewitz-author-div-circle"
+);
+const superintelligenceAuthorProfile = document.querySelector(
+  ".nick-bostrom-author-div-circle"
+);
+const life30Quote = document.querySelector(".life30-elon-quote");
+const onWarQuote = document.querySelector(".OnWar-elon-quote");
+const superintelligenceQuote = document.querySelector(
+  ".Superintelligence-elon-quote"
+);
+const life30Author = document.querySelector(".max-tegmark-author");
+const onWarAuthor = document.querySelector(".carl-von-clausewitz-author");
+const superintelligenceAuthor = document.querySelector(".nick-bostrom-author");
+const superintelligenceTitle = document.querySelector(
+  ".Superintelligence-title"
+);
+const superintelligenceSubtitle = document.querySelector(
+  ".Superintelligence-subtitle"
+);
+const onWarTitle = document.querySelector(".OnWar-title");
+const onWarSubtitle = document.querySelector(".OnWar-subtitle");
+const life30Title = document.querySelector(".life30-title");
+const life30Subtitle = document.querySelector(".life30-subtitle");
+const clickOnImageMessage = document.querySelector(
+  ".elon-images-story-subtitle"
+);
+const nerdyLogo = document.querySelector(".most-recommended-books-img");
 
 let isOriginalButtonStyle = true;
 
@@ -214,11 +256,176 @@ overallButton.addEventListener("click", function () {
   if (isOriginalButtonStyle) {
     circleButton.style.left = "41px";
     htmlBody.style.backgroundColor = "#000000";
+    headerBorder.style.borderBottom = "1px solid white";
+    guessElonTitle.style.color = "#dadada";
+    netWorthTitle.style.color = "#dadada";
+    elonSummary.style.color = "#dadada";
+    elonMoreInfoLink.style.color = "rgb(197, 13, 178)";
+    elonMoreInfoLink.addEventListener("mouseenter", function () {
+      elonMoreInfoLink.style.color = "rgb(255, 98, 239)";
+    });
+    elonMoreInfoLink.addEventListener("mouseleave", function () {
+      elonMoreInfoLink.style.color = "rgb(255, 6, 230)";
+    });
+    elonProfileX.style.border = "1px solid rgb(255, 0, 230)";
+    elonProfileX.addEventListener("mouseenter", function () {
+      elonProfileX.style.border = "2px solid rgb(255, 0, 230)";
+      elonProfileX.style.top = "-67px";
+      elonProfileX.style.left = "81px";
+    });
+    elonProfileX.addEventListener("mouseleave", function () {
+      elonProfileX.style.border = "1px solid rgb(255, 0, 230)";
+      elonProfileX.style.top = "-66px";
+      elonProfileX.style.left = "82px";
+    });
+    divider.style.color = "white";
+    trueFalseDiv.style.backgroundColor = "#423f3f";
+    life30Div.style.backgroundColor = "#423f3f";
+    life30Quote.style.color = "white";
+    onWarDiv.style.backgroundColor = "#423f3f";
+    onWarQuote.style.color = "white";
+    superintelligenceDiv.style.backgroundColor = "#423f3f";
+    superintelligenceQuote.style.color = "white";
+    life30AuthorProfile.style.border = "2px solid rgb(225, 13, 211)";
+    onWarAuthorProfile.style.border = "2px solid rgb(225, 13, 211)";
+    superintelligenceAuthorProfile.style.border = "2px solid rgb(225, 13, 211)";
+    life30Author.style.color = "rgb(255, 255, 255)";
+    onWarAuthor.style.color = "white ";
+    superintelligenceAuthor.style.color = "white";
+    // life30 title
+    life30Title.style.background =
+      "radial-gradient(rgb(255, 255, 255), rgb(243, 9, 255))";
+    life30Title.style.backgroundSize = "190% 200%";
+    life30Title.style.backgroundPosition = "100% 100%";
+    life30Title.style.backgroundClip = "text";
+    life30Title.style.webkitTextFillColor = "transparent";
+    life30Title.style.marginTop = "16px";
+    life30Title.style.paddingBottom = "12px";
+    // OnWar title
+    onWarTitle.style.background =
+      "radial-gradient(rgb(255, 255, 255), rgb(243, 9, 255))";
+    onWarTitle.style.backgroundSize = "190% 200%";
+    onWarTitle.style.backgroundPosition = "100% 100%";
+    onWarTitle.style.backgroundClip = "text";
+    onWarTitle.style.webkitTextFillColor = "transparent";
+    onWarTitle.style.marginTop = "16px";
+    onWarTitle.style.paddingBottom = "12px";
+    // Superintelligence title
+    superintelligenceTitle.style.background =
+      "radial-gradient(rgb(255, 255, 255), rgb(243, 9, 255))";
+    superintelligenceTitle.style.backgroundSize = "190% 200%";
+    superintelligenceTitle.style.backgroundPosition = "100% 100%";
+    superintelligenceTitle.style.backgroundClip = "text";
+    superintelligenceTitle.style.webkitTextFillColor = "transparent";
+    superintelligenceTitle.style.marginTop = "16px";
+    superintelligenceTitle.style.paddingBottom = "12px";
+    // life30 subtitle
+    life30Subtitle.style.background =
+      "radial-gradient(rgb(243, 9, 255), rgb(255, 255, 255))";
+    life30Subtitle.style.backgroundSize = "190% 200%";
+    life30Subtitle.style.backgroundPosition = "100% 100%";
+    life30Subtitle.style.backgroundClip = "text";
+    life30Subtitle.style.webkitTextFillColor = "transparent";
+    // onwar subtitle
+    onWarSubtitle.style.background =
+      "radial-gradient(rgb(243, 9, 255), rgb(255, 255, 255))";
+    onWarSubtitle.style.backgroundSize = "190% 200%";
+    onWarSubtitle.style.backgroundPosition = "100% 100%";
+    onWarSubtitle.style.backgroundClip = "text";
+    onWarSubtitle.style.webkitTextFillColor = "transparent";
+    // superintelligence subtitle
+    superintelligenceSubtitle.style.background =
+      "radial-gradient(rgb(243, 9, 255), rgb(255, 255, 255))";
+    superintelligenceSubtitle.style.backgroundSize = "190% 200%";
+    superintelligenceSubtitle.style.backgroundPosition = "100% 100%";
+    superintelligenceSubtitle.style.backgroundClip = "text";
+    superintelligenceSubtitle.style.webkitTextFillColor = "transparent";
+
+    clickOnImageMessage.style.color = "white";
+    userLoginDiv.style.backgroundColor = "#423f3f";
+    userLoginQuestion.style.color = "white";
+    nerdyLogo.style.cssText = "transition: opacity 0.1s; opacity: 0";
+    setTimeout(() => {
+      nerdyLogo.src = "images/nerdyLogoBlack.png";
+      nerdyLogo.style.opacity = "1";
+    }, 500);
   } else {
+    nerdyLogo.style.cssText = "transition: opacity 0.1s; opacity: 0";
+    setTimeout(() => {
+      nerdyLogo.src = "images/nerdyLogoWhite.png";
+      nerdyLogo.style.opacity = "1";
+    }, 500);
     circleButton.style.left = "4px";
     htmlBody.style.backgroundColor = "transparent";
+    headerBorder.style.borderBottom = "1px solid black";
+    guessElonTitle.style.color = "#000000";
+    netWorthTitle.style.color = "#000000";
+    elonSummary.style.color = "#000000";
+    elonMoreInfoLink.style.color = "1px solid transparent";
+    divider.style.color = "black";
+    trueFalseDiv.style.backgroundColor = "#dadada";
+    life30Div.style.backgroundColor = "rgb(220, 220, 220)";
+    onWarDiv.style.backgroundColor = "rgb(220, 220, 220)";
+    superintelligenceDiv.style.backgroundColor = "rgb(220, 220, 220)";
+    onWarQuote.style.color = "#000000";
+    superintelligenceQuote.style.color = "#000000";
+    life30Quote.style.color = "#000000";
+    life30Author.style.color = "rgb(117, 32, 132)";
+    onWarAuthor.style.color = "rgb(117, 32, 132)";
+    superintelligenceAuthor.style.color = "rgb(117, 32, 132)";
+    // Life30 title
+    life30Title.style.background =
+      "radial-gradient(rgb(165, 27, 163), rgb(98, 8, 125))";
+    life30Title.style.backgroundSize = "190% 200%";
+    life30Title.style.backgroundPosition = "100% 100%";
+    life30Title.style.backgroundClip = "text";
+    life30Title.style.webkitTextFillColor = "transparent";
+    life30Title.style.marginTop = "16px";
+    life30Title.style.paddingBottom = "12px";
+    // OnWar title
+    onWarTitle.style.background =
+      "radial-gradient(rgb(165, 27, 163), rgb(98, 8, 125))";
+    onWarTitle.style.backgroundSize = "190% 200%";
+    onWarTitle.style.backgroundPosition = "100% 100%";
+    onWarTitle.style.backgroundClip = "text";
+    onWarTitle.style.webkitTextFillColor = "transparent";
+    onWarTitle.style.marginTop = "16px";
+    onWarTitle.style.paddingBottom = "12px";
+    // Superintelligence title
+    superintelligenceTitle.style.background =
+      "radial-gradient(rgb(165, 27, 163), rgb(98, 8, 125))";
+    superintelligenceTitle.style.backgroundSize = "190% 200%";
+    superintelligenceTitle.style.backgroundPosition = "100% 100%";
+    superintelligenceTitle.style.backgroundClip = "text";
+    superintelligenceTitle.style.webkitTextFillColor = "transparent";
+    superintelligenceTitle.style.marginTop = "16px";
+    superintelligenceTitle.style.paddingBottom = "12px";
+    // Life30 subtitle
+    life30Subtitle.style.background =
+      "radial-gradient(rgb(255, 111, 255), rgb(38, 1, 50))";
+    life30Subtitle.style.backgroundSize = "190% 200%";
+    life30Subtitle.style.backgroundPosition = "100% 100%";
+    life30Subtitle.style.backgroundClip = "text";
+    life30Subtitle.style.webkitTextFillColor = "transparent";
+    // Onwar subtitle
+    onWarSubtitle.style.background =
+      "radial-gradient(rgb(255, 111, 255), rgb(38, 1, 50))";
+    onWarSubtitle.style.backgroundSize = "190% 200%";
+    onWarSubtitle.style.backgroundPosition = "100% 100%";
+    onWarSubtitle.style.backgroundClip = "text";
+    onWarSubtitle.style.webkitTextFillColor = "transparent";
+    // Superintelligence subtitle
+    superintelligenceSubtitle.style.background =
+      "radial-gradient(rgb(255, 111, 255), rgb(38, 1, 50))";
+    superintelligenceSubtitle.style.backgroundSize = "190% 200%";
+    superintelligenceSubtitle.style.backgroundPosition = "100% 100%";
+    superintelligenceSubtitle.style.backgroundClip = "text";
+    superintelligenceSubtitle.style.webkitTextFillColor = "transparent";
+
+    clickOnImageMessage.style.color = "black";
+    userLoginDiv.style.backgroundColor = "rgb(202, 191, 191)";
+    userLoginQuestion.style.color = "black";
   }
 
   isOriginalButtonStyle = !isOriginalButtonStyle;
 });
-
